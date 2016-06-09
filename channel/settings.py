bdbc2bcc52acf53dfd2fb5002df35ef1e3665b6c
Emett Speer <speer.emett@gmail.com>
2016-06-09 06:55:26 -0700

    Rolled back to before heroku

--- channel/settings.py
+++ channel/settings.py
@@ -158,18 +158,6 @@ class ProductionSettings(BaseSettings):
         },
     }
 
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, '/static/')
-    STATIC_URL = '/static/'
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-
 
 MODE = os.environ.get('DJANGO_MODE', 'Base')
 cbs.apply('{}Settings'.format(MODE.title()), globals())
