c3536a718a494afacd80a63aa18c4bcc09fbb9b0
Emett Speer <speer.emett@gmail.com>
2016-06-09 04:03:41 -0700

    Updated static file settings

--- channel/settings.py
+++ channel/settings.py
@@ -162,13 +162,11 @@ class ProductionSettings(BaseSettings):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATIC_URL = '/static/'
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-                os.path.join(PROJECT_ROOT, 'static'),
-                )
+    STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
 
 MODE = os.environ.get('DJANGO_MODE', 'Base')
