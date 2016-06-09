378bd02404769016ffc7fce3202bc834b823a47b
Emett Speer <speer.emett@gmail.com>
2016-06-08 23:33:36 -0700

    Switched to function based consumers and its working now

--- channel/routing.py
+++ channel/routing.py
@@ -2,8 +2,7 @@ from channels.routing import route, route_class
 from chat import consumers
 
 channel_routing = [
-    route_class(consumers.ChatServer, path=r'^/chat/$'),
-    # route("websocket.connect", consumers.ws_connect),
-    # route("websocket.receive", consumers.ws_message),
-    # route("websocket.disconnect", consumers.ws_disconnect),
+    route("websocket.connect", consumers.ws_connect),
+    route("websocket.receive", consumers.ws_message),
+    route("websocket.disconnect", consumers.ws_disconnect),
 ]
