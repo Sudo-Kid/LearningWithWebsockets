7d99c7ac85b4680c6232458348cd01a02d9db89a
Emett Speer <speer.emett@gmail.com>
2016-06-08 21:37:51 -0700

    More updates

--- channel/routing.py
+++ channel/routing.py
@@ -1,8 +1,9 @@
-from channels.routing import route
+from channels.routing import route, route_class
 from chat import consumers
 
 channel_routing = [
-    route("websocket.connect", consumers.ws_connect),
-    route("websocket.receive", consumers.ws_message),
-    route("websocket.disconnect", consumers.ws_disconnect),
+    route_class(consumers.ChatServer, path=r'^/chat/$'),
+    # route("websocket.connect", consumers.ws_connect),
+    # route("websocket.receive", consumers.ws_message),
+    # route("websocket.disconnect", consumers.ws_disconnect),
 ]
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,38 @@
 from channels import Group
 from channels.sessions import channel_session
+from channels.generic.websockets import JsonWebsocketConsumer
 
 
-@channel_session
-def ws_connect(message):
-    room = message.content['path'].strip("/")
-    message.channel_session['room'] = room
-    Group("chat-%s" % room).add(message.reply_channel)
+class ChatServer(JsonWebsocketConsumer):
+    strict_ordering = False
+    slight_ordering = True
 
-@channel_session
-def ws_message(message):
-    Group("chat-%s" % message.channel_session['room']).send({
-        "text": message['text'],
-    })
+    def receive(self, content, **kwargs):
+        print('receive', content)
+        self.send(content)
 
-@channel_session
-def ws_disconnect(message):
-    Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
+    def send(self, content, **kwargs):
+        print('send', content)
+        super(JsonWebsocketConsumer, self).send(content)
+
+
+
+
+# @channel_session
+# def ws_connect(message):
+#     print('ws_connect', message)
+#     room = message.content['path'].strip("/")
+#     message.channel_session['room'] = room
+#     Group("chat-%s" % room).add(message.reply_channel)
+#
+# @channel_session
+# def ws_message(message):
+#     print('ws_message', message)
+#     Group("chat-%s" % message.channel_session['room']).send({
+#         "text": message['text'],
+#     })
+#
+# @channel_session
+# def ws_disconnect(message):
+#     print('ws_disconnect', message)
+#     Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
