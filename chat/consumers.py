807d41ba476f2933f5e1a4374a6a7e3c21a21576
Emett Speer <es@gobrinson.com>
2016-06-13 21:27:53 -0700

    Added group prems

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,10 +5,16 @@ from channels.auth import http_session_user
 from channels.auth import channel_session_user
 from channels.auth import channel_session_user_from_http
 
+from user_profile import models
 
-def select_room(request):
+
+def select_group(request):
+    group = request.content['path'].strip('/')
     if request.user.is_authenticated() and request.user.is_active:
-        return ['auth-chat']
+        user_groups = models.UserChatGroup.objects.filter(user__user=request.user, group__name=group)
+        if not user_groups:
+            return ['auth-chat']
+        return [user_groups[0].group.name]
     else:
         return ['no-auth-chat']
 
@@ -18,33 +24,33 @@ def add_user_to_message(message):
     content['user'] = user
     return json.dumps(content)
 
-def new_user_in_channel(message):
+def new_user_in_channel(message, group):
     user = message.user.get_username()
     content = {
-        'user': 'Bot',
-        'msgText': user + ' has joined the channel'
+        'user': 'Bot' + '@' + group[0],
+        'msgText': user + ' has joined the group ' + group[0]
     }
     return json.dumps(content)
 
 @channel_session_user_from_http
 def ws_connect(message):
-    room = select_room(message)
-    content = new_user_in_channel(message)
-    Group("chat-%s" % room).add(message.reply_channel)
-    Group("chat-%s" % room).send({
+    group = select_group(message)
+    content = new_user_in_channel(message, group)
+    Group("chat-%s" % group).add(message.reply_channel)
+    Group("chat-%s" % group).send({
         "text": content,
     })
 
 @channel_session_user
 def ws_message(message):
-    room = select_room(message)
+    group = select_group(message)
     content = add_user_to_message(message)
-    Group("chat-%s" % room).send({
+    Group("chat-%s" % group).send({
         "text": content,
     })
 
 @channel_session_user
 def ws_disconnect(message):
-    room = select_room(message)
-    Group("chat-%s" % room).discard(
+    group = select_group(message)
+    Group("chat-%s" % group).discard(
             message.reply_channel)
--- chat/views.py
+++ chat/views.py
@@ -6,4 +6,5 @@ class Index(View):
     template_name = 'chat/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+        chat_room = self.kwargs['chat_room']
+        return render(request, self.template_name, {'chat_room': chat_room})
